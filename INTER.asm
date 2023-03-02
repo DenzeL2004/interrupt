fdd3273dd461f1a2d48f05f35ab1bd3dd112310b
DenzeL2004 <work20022004@mail.ru>
2023-03-03 01:30:40 +0300

    frame with registers

--- INTER.asm
+++ INTER.asm
@@ -7,13 +7,30 @@ org 100h
 HACKER_STYLE equ 3Fh;0ah
 FRAME_BUTTON equ 002d ;bscan code button '1'
 WIDTH_LENGTH equ 80d
+HIGHT_WIDTH_FRAME equ 1010h
+
+;saves generals registers to curRegVal
+;-----------------------------------------------------------------
+;Assumes: cs
+;Destroy: di
+;-----------------------------------------------------------------
+SAVE_GEN_REG macro     
+            
+            push di
+            pusha
+
+            mov di, 16d
+            @@next:
+                sub di, 2d
+                pop word ptr cs:[curRegVal + di]
+            cmp di, 0d
+            jne @@next 
+
+            pop di
+
+            endm
 
 Start:
-
-    ;call NewInt09
-
-    ;mov ax, 4c00h
-    ;int 21h
 
     xor di, di
     mov es, di      ;start interrupt table
@@ -41,44 +58,76 @@ Start:
     int 21h
     ;resident memmory
 
-NewInt09 proc
-    pushf           ;save flags
-    push ax cx dx si di es ;save registers
+    ;-----------------------------------------------------------------
+	;Interrupt function for drawing a frame on button click
+	;-----------------------------------------------------------------
+    ;Entry: none
+    ;Exit: none
+    ;Destroy: ax, cx, dx, si, di, es, ds
+	;-----------------------------------------------------------------
+    NewInt09 proc
+        pushf           ;save flags
+        push ax cx dx si di es ;save registers
 
-    in al, 60h    ;read from port 60h
-    cmp al, FRAME_BUTTON
-    jne @@noFrame        
+
+        SAVE_GEN_REG
+
+
+        in al, 60h    ;read from port 60h
+        cmp al, FRAME_BUTTON
+        jne @@noFrame        
+            call RegInfo
+        @@noFrame:
+
+        in al, 61h
+        or al, 80h
+        out 61h, al
+        and al, not 80h
+        out 61h, al     ;ppi command that character was read
+
+        mov al, 20h ;finished the current interrupt
+        out 20h, al ;we can move on to the next interrupt
+
+
+        pop es di si dx cx ax    ;restore registers
+        popf            ;restore flags
+
+        db 0eah     ;far jump
+    Old09Ofs dw 0   ;old offset of 09h interrupt
+    Old09Seg dw 0   ;old segment og 09h interrupt
+        
+    NewInt09 endp
+
+    ;-----------------------------------------------------------------
+	;Draw frame with registers value
+	;-----------------------------------------------------------------
+    ;Assumes: curRegVal - registers value
+    ;Entry: none
+	;Exit: none
+	;Destroy: ax, bx, cx, dx, si, di, df
+	;-----------------------------------------------------------------
+	RegInfo	proc
         mov di, 0b800h
         mov es, di
-        mov di, 160d * 5d + 80d   ;vidmem addres
+        xor di, di   ;vidmem addres
 
-        mov ah, HACKER_STYLE
-        mov word ptr es:[di], ax
-        push 0510h ;hight:width
-        push 0     ;sample
+        push ds
+        mov ax, cs  
+        mov ds, ax  ;preservsave to ds cs toing the case of cs in ds in order for string functions to work correctly
+
+        push HIGHT_WIDTH_FRAME ;hight:width
+
+        push offset frameSample1    ;sample
         push HACKER_STYLE ;style
         call DrawFrame
 
-    @@noFrame:
+        pop ds
 
-    in al, 61h
-    or al, 80h
-    out 61h, al
-    and al, not 80h
-    out 61h, al     ;ppi command that character was read
+        mov word ptr cs:[regAdrPrt], WIDTH_LENGTH * 1d * 2d + 1d * 2d ;shift 
+        call PrintReg
 
-    mov al, 20h ;finished the current interrupt
-    out 20h, al ;we can move on to the next interrupt
-
-
-    pop es di si dx cx ax    ;restore registers
-    popf            ;restore flags
-
-    db 0eah     ;far jump
-Old09Ofs dw 0   ;old offset of 09h interrupt
-Old09Seg dw 0   ;old segment og 09h interrupt
-    
-NewInt09 endp
+        ret
+	RegInfo	endp    
 
     ;-----------------------------------------------------------------
 	;Draw frame in video memory (pascal)
@@ -163,6 +212,47 @@ NewInt09 endp
 	DrawLine	endp
 
     ;-----------------------------------------------------------------
+	;Print cur value from General regisers
+	;-----------------------------------------------------------------
+    ;Assumes: regAdrPrt = entry address, curRegVal - registers value
+    ;Entry: none
+	;Exit: none
+	;Destroy: ax, bx, cx, dx, si, di, df
+	;-----------------------------------------------------------------
+	PrintReg	proc
+        cld 		;DF = 0
+        
+        mov di, word ptr cs:[regAdrPrt] ;set in di entry addres
+
+        mov ah, HACKER_STYLE
+
+        xor bx, bx
+        @@next:
+            mov dx, word ptr cs:[curRegVal + bx]
+            add di, 8d    ;shift by 3 cell
+            call HexRep
+
+            add di, 14d   ;shift by 7 cell
+            
+            mov al, byte ptr cs:[regGenName + bx]
+            stosw
+            inc bx
+
+            mov al, byte ptr cs:[regGenName + bx]
+            stosw
+            inc bx
+
+            add di, WIDTH_LENGTH * 2d  ;next line
+            sub di, 10d                ;table alignment
+
+        
+        cmp bx, 16d ;dra all registers
+        jne @@next
+
+        ret
+	PrintReg	endp
+
+    ;-----------------------------------------------------------------
 	;hex representation of a number
 	;-----------------------------------------------------------------
 	;The number is printed from right to left.
@@ -180,7 +270,7 @@ NewInt09 endp
 		@@next:
 			mov si, 000fh               ;hex mask
 			and si, dx			        ;get last digit
-			mov al, cs:[hex_code + si]  ;suppose cur symbol is num
+			mov al, cs:[hexCode + si]  ;suppose cur symbol is num
 
 			stosw
 
@@ -189,8 +279,10 @@ NewInt09 endp
 
 		ret
 	HexRep	endp
-hex_code db "0123456789ABCDEF"
-
+hexCode db "0123456789ABCDEF" ;hex represenation
+regGenName dw "ax", "bx", "cx", "dx", "si", "di", "sp", "dp"    ;registers name
+regAdrPrt  dw 0 ;address where to write register values
+curRegVal  dw 8 dup (0) ;current registers value
 frameSample1 db 0dah, 0c4h, 0bfh, 0c3h, 20h, 0b4h, 0c0h, 0c4h, 0d9h  ;patern for frame
 
 ProgramEnd:
