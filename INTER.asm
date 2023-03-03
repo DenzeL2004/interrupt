3f1699cac734184ad96cde5564f85483d7d554db
DenzeL2004 <work20022004@mail.ru>
2023-03-03 19:54:11 +0300

    add draw buffer

--- INTER.asm
+++ INTER.asm
@@ -6,21 +6,30 @@ org 100h
 
 HACKER_STYLE equ 0ah
 FRAME_BUTTON equ 002d ;bscan code button '1'
-WIDTH_LENGTH equ 80d
-HIGHT_WIDTH_FRAME equ 1010h
+
+WIDTH_VID_MEM equ 80d
+
+HIGHT_FRAME equ 0Eh
+WIDTH_FRAME equ 0Bh
+
 BYTE_TRUE equ 0FFh
+
+CNT_REGISTERS equ 12d
+
+BUFFER_SIZE equ 200d
 
 ;saves generals registers to curRegVal
 ;-----------------------------------------------------------------
 ;Assumes: cs
-;Destroy: di
+;Destroy: none
 ;-----------------------------------------------------------------
 SAVE_GEN_REG macro     
             
             push di
             pusha
+            push ds es ss cs
 
-            mov di, 16d
+            mov di, CNT_REGISTERS * 2d
             @@next:
                 sub di, 2d
                 pop word ptr cs:[curRegVal + di]
@@ -30,6 +39,27 @@ SAVE_GEN_REG macro
             pop di
 
             endm
+
+;-----------------------------------------------------------------
+;Dealy of programm
+;-----------------------------------------------------------------
+;Entry:   none
+;Exit:    none
+;Destroy: none
+;-----------------------------------------------------------------
+Delay   macro cnt
+            
+        push cx
+        
+        xor cx, cx
+        dec cx
+        @@delay:
+            nop
+        loop @@delay
+
+        pop cx
+
+        endm
 
 Start:
 
@@ -82,13 +112,13 @@ Start:
     ;Destroy: ax, cx, dx, si, di, es, ds
 	;-----------------------------------------------------------------
     NewInt09 proc
-        pushf                   ;save flags
-        push ax cx dx si di es  ;save registers
+        pushf        ;save flags
+        push ax      ;save registers
 
         in al, 60h    ;read from port 60h
         cmp al, FRAME_BUTTON
         jne @@noFrame        
-            mov cs:[flagEntryButton], BYTE_TRUE    ;button was entry
+            not cs:[flagEntryButton]    ;button was entry
         @@noFrame:
 
         in al, 61h
@@ -100,9 +130,8 @@ Start:
         mov al, 20h ;finished the current interrupt
         out 20h, al ;we can move on to the next interrupt
 
-
-        pop es di si dx cx ax   ;restore registers
-        popf                    ;restore flags
+        pop ax   ;restore registers
+        popf        ;restore flags
 
         db 0eah     ;far jump
     Old09Ofs dw 0   ;old offset of 09h interrupt
@@ -147,19 +176,20 @@ Start:
 	;-----------------------------------------------------------------
     NewInt08 proc
         pushf                   ;save flags
-        push ax cx dx si di es  ;save registers
+        push ax bx cx dx si di es ds  ;save registers
 
         SAVE_GEN_REG
 
         cmp byte ptr cs:[flagEntryButton], BYTE_TRUE
-        jne @@noFrame        
+        jne @@noFrame   
             call RegInfo
+            Delay
         @@noFrame:
 
         mov al, 20h ;finished the current interrupt
         out 20h, al ;we can move on to the next interrupt
 
-        pop es di si dx cx ax   ;restore registers
+        pop ds es di si dx cx bx ax   ;restore registers
         popf                    ;restore flags
 
         db 0eah     ;far jump
@@ -171,39 +201,112 @@ Start:
     ;-----------------------------------------------------------------
 	;Draw frame with registers value
 	;-----------------------------------------------------------------
-    ;Assumes: curRegVal - registers value
-    ;Entry: none
+    ;Entry:none
 	;Exit: none
-	;Destroy: ax, bx, cx, dx, si, di, df
+	;Destroy: ax, cx, dx, si, di, df
 	;-----------------------------------------------------------------
 	RegInfo	proc
+
+        SAVE_GEN_REG
+       
+        call printToDrawBuffer
+
         mov di, 0b800h
-        mov es, di
-        xor di, di   ;vidmem addres
+        mov es, di      ;set destination segment
 
-        push ds
-        mov ax, cs  
-        mov ds, ax  ;preservsave to ds cs toing the case of cs in ds in order for string functions to work correctly
+        xor di, di      ;set destination start addres
 
-        push HIGHT_WIDTH_FRAME ;hight:width
+        mov si, cs
+        mov ds, si      ;set source segment
 
-        push offset frameSample1    ;sample
-        push HACKER_STYLE ;style
-        call DrawFrame
+        mov si, offset drawBuffer      ;set source start addres
 
-        pop ds
+        push WIDTH_FRAME * 2d
+        push WIDTH_VID_MEM * 2d
+        push HIGHT_FRAME
 
-        mov word ptr cs:[regAdrPrt], WIDTH_LENGTH * 1d * 2d + 1d * 2d ;shift 
-        call PrintReg
+        call coppyBuffer
 
         ret
 	RegInfo	endp    
+
+    ;-----------------------------------------------------------------
+	;Draw in drawbuffer
+	;-----------------------------------------------------------------
+    ;Assumes: curRegVal - registers value
+    ;Entry: none
+	;Exit: none
+	;Destroy: ax, cx, dx, si, di, df
+	;-----------------------------------------------------------------
+	printToDrawBuffer	proc
+        mov  di, cs
+        
+        push es
+        mov es, di  ;change segment to correct work strok functions
+
+        push ds 
+        mov ds, di  ;change segment to correct work strok functions
+
+        mov di, offset drawBuffer   ;draw buffer addres
+
+        push WIDTH_FRAME * 2d       ;drawDuffer's shift
+        push (HIGHT_FRAME shl 8) or WIDTH_FRAME) ;hight:width
+
+        push offset frameSample1    ;sample
+        push HACKER_STYLE           ;style
+        call DrawFrame
+
+        mov word ptr cs:[regAdrPrt], (offset drawBuffer + WIDTH_FRAME * 1d * 2d + 1d * 2d) ;shift 
+
+        push WIDTH_FRAME * 2d       ;shift
+        call PrintReg
+
+        pop ds es     ;recover ds es
+
+        ret
+	printToDrawBuffer	endp  
+
+    ;-----------------------------------------------------------------
+	;Coppy chanck of memory 
+	;-----------------------------------------------------------------
+    ;Param: [bp + 8] - width chanck source, [bp + 6] - width chanck destination,  
+    ;       [bp + 4] - count of chancks  
+    ;Warning: width chanck source must be less than width chanck destination
+    ;Assumes: es - destination buffer, ds - source buffer
+    ;Entry: si - index sorurce byffer, di - index destination buffer
+	;Exit: none
+	;Destroy: si, di, cx, df
+	;-----------------------------------------------------------------
+	coppyBuffer	proc
+        push bp
+		mov bp, sp
+
+        cld 		;DF = 0
+		
+        @@next:
+
+            mov cx, word ptr [bp + 8]   ;counter
+            shr cx, 1d                  ;cx /= 2 because width chanc mul 2(for correct recalculation) 
+
+            rep movsw                   ;coppy
+
+            sub di, word ptr [bp + 8]   ;------------------
+            add di, word ptr [bp + 6]   ;address correction
+
+        dec word ptr [bp + 4]
+        cmp word ptr [bp + 4], 0        ;if count of chancks equel 0
+        jne @@next
+		
+		pop bp
+		ret 2d * 3d
+	coppyBuffer	endp  
 
     ;-----------------------------------------------------------------
 	;Draw frame in video memory (pascal)
 	;-----------------------------------------------------------------
 	;Param: [bp + 4] - frame's color, [bp + 6] - frame's sample
 	;		[bp + 8] - frame's width, [bp + 9] - frame's highth
+    ;       [bp + 10] - shift in buffer
 	;Assumes: es = VIDMEM_ADR, ds = sourse segment
 	;Entry: di (destination index), si (source index)
 	;Exit: none
@@ -225,7 +328,7 @@ Start:
 		shl cl, 1d                  ;cx * 2
 
 		sub di, cx	;return di to start line
-		add di, WIDTH_LENGTH * 2d ;next line
+		add di, word ptr [bp + 10] ;next line
 
 		sub byte ptr [bp + 9], 2d 	;hight -= 2
 
@@ -239,7 +342,7 @@ Start:
 			shl cl, 1d                  ;cx * 2
 			
 			sub di, cx	;return di to start line
-			add di, WIDTH_LENGTH * 2d ;next line
+			add di, word ptr [bp + 10] ;next line
 			
 		dec byte ptr [bp + 9] 		;hight--
 		cmp byte ptr [bp + 9], 0d	;-----------------------
@@ -251,14 +354,14 @@ Start:
 		call DrawLine
 
 		pop bp
-		ret 2d * 3d
+		ret 2d * 4d
 		
 	DrawFrame	endp
 
 	;-----------------------------------------------------------------
 	;Draw line in video memory from source
 	;-----------------------------------------------------------------
-	;Assumes: es = VIDMEM_ADR, ds = sourse sigment
+	;Assumes: es = VIDMEM_ADR, ds = sourse segment
 	;Entry: ah(frame's color), cx (counter)
 	;Exit: none
 	;Destroy: al, cx, di, si, df
@@ -282,14 +385,18 @@ Start:
 	DrawLine	endp
 
     ;-----------------------------------------------------------------
-	;Print cur value from General regisers
+	;Print cur value from regisers
 	;-----------------------------------------------------------------
+    ;Param: [bp + 4] - shift in buffer
     ;Assumes: regAdrPrt = entry address, curRegVal - registers value
     ;Entry: none
 	;Exit: none
 	;Destroy: ax, bx, cx, dx, si, di, df
 	;-----------------------------------------------------------------
 	PrintReg	proc
+        push bp
+		mov bp, sp
+
         cld 		;DF = 0
         
         mov di, word ptr cs:[regAdrPrt] ;set in di entry addres
@@ -312,14 +419,14 @@ Start:
             stosw
             inc bx
 
-            add di, WIDTH_LENGTH * 2d  ;next line
-            sub di, 10d                ;table alignment
+            add di, word ptr [bp + 4]  ;next line
+            sub di, 10d                 ;table alignment
 
-        
-        cmp bx, 16d ;dra all registers
+        cmp bx, CNT_REGISTERS * 2d ;dra all registers
         jne @@next
 
-        ret
+        pop bp
+		ret 2d * 1d
 	PrintReg	endp
 
     ;-----------------------------------------------------------------
@@ -349,12 +456,19 @@ Start:
 
 		ret
 	HexRep	endp
+
 hexCode db "0123456789ABCDEF" ;hex represenation
+
 flagEntryButton db 0
-regGenName dw "ax", "bx", "cx", "dx", "si", "di", "sp", "dp"    ;registers name
+
+regGenName dw "ax", "bx", "cx", "dx", "si", "di", "sp", "dp", "ds", "es", "ss", "cs"    ;registers name
 regAdrPrt  dw 0 ;address where to write register values
-curRegVal  dw 8 dup (0) ;current registers value
+curRegVal  dw CNT_REGISTERS dup (0) ;current registers value
+
 frameSample1 db 0dah, 0c4h, 0bfh, 0c3h, 20h, 0b4h, 0c0h, 0c4h, 0d9h  ;patern for frame
+
+saveBuffer dw BUFFER_SIZE dup (0)
+drawBuffer dw BUFFER_SIZE dup (0)
 
 ProgramEnd:
 
